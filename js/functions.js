5402f0ff125a8fe05152b8e7bdbdf4f29d5e3cab
Jeremy Bissonnette <jeremy@kwmassage.com>
2016-01-13 20:23:43 -0500

    no paralax ie - remove class js

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,41 @@
+/**
+ * detect IE
+ * returns version of IE or false, if browser is not Internet Explorer
+ */
+function detectIE() {
+    var ua = window.navigator.userAgent;
+
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+        // IE 10 or older => return version number
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    // other browser
+    return false;
+}
+
+if (!detectIE) {
+	if (document.getElementById("slider")) {
+		document.getElementById("slider").classList.remove("slider-parallax");
+	};
+	if (document.getElementById("slider")){
+		document.getElementById("slider").classList.remove("page-title-parallax");
+	};
+	if (document.getElementById("testi_parallax")){
+		document.getElementById("testi_parallax").classList.remove("parallax");
+	};
+};
+
+
+
 var $ = jQuery.noConflict();
 
 $.fn.inlineStyle = function (prop) {
