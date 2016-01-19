5fdb181ee176e674f04e10849588a189171444a3
Jeremy Bissonnette <jeremy@kwmassage.com>
2016-01-19 11:39:12 -0500

    1

--- js/functions.js
+++ js/functions.js
@@ -2,24 +2,17 @@
  * detect IE
  * returns version of IE or false, if browser is not Internet Explorer
  */
-function detectIE() {
+var detectIE = function () {
+
     var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
 
-    var msie = ua.indexOf('MSIE ');
-    if (msie > 0) {
-        // IE 10 or older => return version number
-        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    if (msie > 0) {     // If Internet Explorer, return version number
+        return true;
+    } else  {                 // If another browser, return 0
+		return false;
     }
 
-    var trident = ua.indexOf('Trident/');
-    if (trident > 0) {
-        // IE 11 => return version number
-        var rv = ua.indexOf('rv:');
-        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-    }
-
-    // other browser
-    return false;
 }
 /*
 END - detect ie
